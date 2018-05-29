ec97b29388f978fa2190b7c4d3e451b7a8c40fc8
sevacom <sevacom@hotmail.com>
2018-05-29 23:49:53 +0700

    fix nuspec and version

--- Moq.AutoMock/Properties/AssemblyInfo.cs
+++ Moq.AutoMock/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Moq.AutoMocker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An auto-mocking container that generates mocks using Moq")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Moq.AutoMocker")]
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.1.0")]
-[assembly: AssemblyFileVersion("0.4.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("Moq.AutoMock.Tests")]
